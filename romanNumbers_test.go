0ad03a125e5ffff7283f357c09c147749a3b92c5
Clebson Moura <clebson.sh@gmail.com>
2024-02-14 15:50:34 -0300

    Add unit test for ConvertToRomanNumber function testing if 1 can be converted o I

---
+++ romanNumbers_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func TestConvertToRomanNumber(t *testing.T) {
+	numbersToTest := []struct {
+		arabicNumber int
+		romanNumber  string
+	}{
+		{1, "I"},
+	}
+
+	for _, tt := range numbersToTest {
+		assertConvertToRomanNumber(t, tt.arabicNumber, tt.romanNumber)
+	}
+
+}
+
+func assertConvertToRomanNumber(t *testing.T, arabicNumber int, want string) {
+	t.Helper()
+	got := ConvertToRomanNumber(arabicNumber)
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
