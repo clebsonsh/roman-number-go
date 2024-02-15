9878bc32e2b70c7cb443e619cb216dc571890a03
Clebson Moura <clebson.sh@gmail.com>
2024-02-15 09:39:54 -0300

    Refactor ConvertToRomanNumber function to use a struct for arabicToRoman mapping

--- romanNumbers.go
+++ romanNumbers.go
@@ -1,18 +1,21 @@
 package main
 
 func ConvertToRomanNumber(arabicNumber int) string {
-	arabicToRoman := map[int]string{
-		5: "V",
-		4: "IV",
-		1: "I",
+	arabicToRoman := []struct {
+		arabic int
+		roman  string
+	}{
+		{5, "V"},
+		{4, "IV"},
+		{1, "I"},
 	}
 
 	result := ""
 
-	for arabic, roman := range arabicToRoman {
-		for arabicNumber >= arabic {
-			result += roman
-			arabicNumber -= arabic
+	for _, current := range arabicToRoman {
+		for arabicNumber >= current.arabic {
+			result += current.roman
+			arabicNumber -= current.arabic
 		}
 	}
 
